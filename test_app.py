a16e745c199e980cf9427f58884fd8b652449ad4
gmkeerthi <145956801+gmkeerthi@users.noreply.github.com>
2025-08-20 10:07:22 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
-def add(a,b):
-  return a+b
+from app import add   # import from app.py
 
-add(5,6)
-add(8,9)
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
